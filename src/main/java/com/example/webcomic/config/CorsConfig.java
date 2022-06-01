46d8d269a3a10880ba1c156ba71c69bf3d2a8a10
Tien01els <tien140801@gmail.com>
2022-06-01 16:53:46 +0700

    Update CorsConfig.java

--- src/main/java/com/example/webcomic/config/CorsConfig.java
+++ src/main/java/com/example/webcomic/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
-                        .allowedOrigins("https://web-comic-application.herokuapp.com/");
+                        .allowedOrigins("*");
             }
         };
     }
