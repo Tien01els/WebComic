4b7fc6ed8dc4a87c9f0c4a7f165030e0a3b84852
Tien01els <tien140801@gmail.com>
2022-06-05 18:39:31 +0700

    Update AccountServiceImpl.java

--- src/main/java/com/example/webcomic/services/account/AccountServiceImpl.java
+++ src/main/java/com/example/webcomic/services/account/AccountServiceImpl.java
@@ -107,7 +107,7 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public ResponseObject changePassword(String idUser, String oldPassword, String newPassword) {
         Optional<Account> account = accountRepository.findAccountById(idUser);
-        if (PasswordEncryptionSingleton.getInstance().compare(oldPassword, account.get().getPassword())) {
+        if (!PasswordEncryptionSingleton.getInstance().compare(oldPassword, account.get().getPassword())) {
             return new ResponseObject("Fail", "The old password does not match the current password", "");
         } else if (PasswordEncryptionSingleton.getInstance().compare(newPassword, account.get().getPassword())) {
             return new ResponseObject("Fail", "The new password is the same as the old password", "");
